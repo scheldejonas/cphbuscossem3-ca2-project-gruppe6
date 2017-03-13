237e8a557b9733cfe296f4cd50a1428c0230a7aa
Schelde Jonas <scheldejonas@gmail.com>
2017-03-13 17:12:25 +0100

    Updated Phone Dao

--- src/main/java/phone/PhoneResource.java
+++ src/main/java/phone/PhoneResource.java
@@ -23,11 +23,10 @@ public class PhoneResource {
     }
 
     @GET
-    @Path("/{addressAmount}")
+    @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getRandomAddress(@PathParam("addressAmount") String addressAmount, @QueryParam("arguments") String arguments) {
-        System.out.println("Testing amount recieved through pathparam: " + addressAmount);
-        System.out.println("Testing arguments recieved through p athparam: " + arguments);
+    public Response getRandomAddress(@QueryParam("arguments") String arguments) {
+        System.out.println("Testing amount recieved through Header or Path Arguments: " + arguments);
         return Response
                 .status(200)
                 .header("Access-Control-Allow-Origin", "*")
@@ -35,9 +34,7 @@ public class PhoneResource {
                 .header("Access-Control-Allow-Credentials", "true")
                 .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
                 .header("Access-Control-Max-Age", "1209600")
-                .entity(
-                        // TODO: ælasdkfj
-                )
+                .entity(PhoneDao.getSingleton().findAll())
                 .build();
 
     }
