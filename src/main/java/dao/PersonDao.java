09be2c57eb52ce6b385ef3102b999b545d0370fb
Viterkim <viktorkim2005@hotmail.com>
2017-03-17 12:11:49 +0100

    The dangerous crocodile

--- src/main/java/dao/PersonDao.java
+++ src/main/java/dao/PersonDao.java
@@ -13,63 +13,76 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonDao {
-
+public class PersonDao
+{
+    
     EntityManager manager;
-
-    public PersonDao(EntityManager manager) {
+    
+    public PersonDao(EntityManager manager)
+    {
         this.manager = manager;
     }
-
-    public ArrayList<Person> findPeopleFromZipcode(String zipcode) {
+    
+    public ArrayList<Person> findPeopleFromZipcode(String zipcode)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p WHERE p.address.cityInfo.zipCode = :zipCode");
         q.setParameter("zipCode", zipcode);
         return (ArrayList<Person>) q.getResultList();
     }
-
-    public ArrayList<Person> findPeopleFromAddress(String street) {
+    
+    public ArrayList<Person> findPeopleFromAddress(String street)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p WHERE p.address.street = :street");
         q.setParameter("street", street);
         return (ArrayList<Person>) q.getResultList();
     }
-
-    public ArrayList<Person> findPeopleFromPhone(String phone) {
+    
+    public ArrayList<Person> findPeopleFromPhone(String phone)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p, Phone ph WHERE ph.info.id = p.id AND ph.number = :phone");
         q.setParameter("phone", phone);
         return (ArrayList<Person>) q.getResultList();
     }
-
-    public ArrayList<Person> findPeopleFromHobby(String hobby) {
+    
+    public ArrayList<Person> findPeopleFromHobby(String hobby)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p, Hobby h WHERE p MEMBER OF h.people AND h.name = :hobby");
         q.setParameter("hobby", hobby);
         return (ArrayList<Person>) q.getResultList();
     }
     
-    public ArrayList<Person> findPeopleFromName(String name) {
+    public ArrayList<Person> findPeopleFromName(String name)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p WHERE p.firstName = :name OR p.lastName = :name");
         q.setParameter("name", name);
         return (ArrayList<Person>) q.getResultList();
     }
-
-    public Person getPersonByID(String id) {
+    
+    public Person getPersonByID(String id)
+    {
         Query q = manager.createQuery("SELECT p FROM Person p WHERE p.id = :id");
         q.setParameter("id", Integer.parseInt(id));
         return (Person) q.getSingleResult();
     }
-
-    public CityInfo findSingleCity(String zipCode) {
+    
+    public CityInfo findSingleCity(String zipCode)
+    {
         Query q = manager.createQuery("SELECT i FROM CityInfo i WHERE i.zipCode = :zipCode");
         q.setParameter("zipCode", zipCode);
         return (CityInfo) q.getSingleResult();
     }
-
-    public void createPerson(Person person) throws ServerException {
-        try {
+    
+    public void createPerson(Person person) throws ServerException
+    {
+        try
+        {
             manager.getTransaction().begin();
             manager.persist(person);
             manager.getTransaction().commit();
-        } catch (Exception exception) {
+        } catch (Exception exception)
+        {
             manager.getTransaction().rollback();
             throw new ServerException();
+        }
     }
 }
